46a7bd19ed9a6629cc561e3a6d88d27c442e1e12
Deivdson Pereira Oliveira da Silva <20211014040037@ifrn.local>
2023-07-06 13:26:42 -0300

    -

---
+++ src/main/java/com/example/ExJPA/ExJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExJpaApplication.class, args);
+	}
+
+}
